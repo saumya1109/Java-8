140d33ec610a26043ca2d8b88603bdac062c577b
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-07-21 15:45:59 -0400

    Create RunnableLambdaExample.java

---
+++ Features/src/Lambdas/RunnableLambdaExample.java
@@ -0,0 +1,39 @@
+package Lambdas;
+public class RunnableLambdaExample {
+
+    public static void main(String[] args) {
+
+        /*Prior Java 8*/
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside Runnable");
+
+            }
+        };
+        new Thread(runnable).start();
+
+        // Java -8  lambda  -
+        // (Lambda Input parameters ) -> Arrow {Lambda Body};
+        Runnable runnableLambda = () -> {
+            System.out.println("Inside Lambda Runnable");
+        };
+        new Thread(runnableLambda).start();
+
+        //One line can be kept even without the brackets {}
+        Runnable runnableLambda1 = () -> System.out.println("Inside Lambda1 Runnable");
+        new Thread(runnableLambda1).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside Runnable 3.1");
+            }
+        });
+
+        new Thread(() -> System.out.println("Inside Lambda Runnable 4")).start();
+
+        
+    }
+}
+
