acf6571da87a47a86de792aa0694fac2ea9718d1
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-07-26 12:24:55 -0400

    Create BinaryOperatorExample.java

---
+++ Features/src/FunctionalInterfaces/BinaryOperatorExample.java
@@ -0,0 +1,20 @@
+package FunctionalInterfaces;
+
+import java.util.Comparator;
+import java.util.function.BinaryOperator;
+
+public class BinaryOperatorExample {
+    static Comparator<Integer> comparator = (a,b) -> a.compareTo(b);
+
+
+    public static void main(String[] args) {
+        BinaryOperator<Integer> binaryOperator = (a, b) -> a*b;
+        System.out.println(binaryOperator.apply(3,5));
+
+        BinaryOperator<Integer> maxBy= BinaryOperator.maxBy(comparator);
+        System.out.println( maxBy.apply(4,5));
+
+        BinaryOperator<Integer> minBy= BinaryOperator.minBy(comparator);
+        System.out.println( minBy.apply(4,5));
+    }
+}
