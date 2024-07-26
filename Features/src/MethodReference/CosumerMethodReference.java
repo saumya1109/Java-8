1caa4894f5eb322be7213a11edb8fa4831251d20
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-07-26 12:25:05 -0400

    Create CosumerMethodReference.java

---
+++ Features/src/MethodReference/CosumerMethodReference.java
@@ -0,0 +1,22 @@
+package MethodReference;
+
+import Data.Student;
+import Data.StudentDataBase;
+
+import java.util.function.Consumer;
+
+public class CosumerMethodReference {
+    static Consumer<Student> c1= (student) -> System.out.println(student);
+
+    //Syntax--> className::methodName
+    static Consumer<Student> c2= System.out::println;
+
+    static Consumer<Student> c3 =Student::printListOfActivities;
+
+    public static void main(String[] args) {
+       StudentDataBase.getAllStudents().forEach(c1);
+        StudentDataBase.getAllStudents().forEach(c2);
+        StudentDataBase.getAllStudents().forEach(c3);
+
+    }
+}
