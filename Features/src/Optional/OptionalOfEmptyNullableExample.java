4a7ab6734dbe48f2f001dac7aa2b4391ad2f3158
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-07-31 15:19:14 -0400

    Update OptionalOfEmptyNullableExample.java

--- Features/src/Optional/OptionalOfEmptyNullableExample.java
+++ Features/src/Optional/OptionalOfEmptyNullableExample.java
@@ -1,4 +1,37 @@
 package Optional;
 
+import java.util.Optional;
+
 public class OptionalOfEmptyNullableExample {
+    public static void main(String[] args) {
+        System.out.println(OfNullable());
+        System.out.println(Of());
+        System.out.println(empty());
+
+    }
+
+    public static Optional<String> OfNullable() {
+
+        //When you are dealing with values that might be null.
+
+        Optional<String> value = Optional.ofNullable("Hello");  //--> Optional[Hello]
+        //Optional<String> value=  Optional.ofNullable(null); // ----> Optional.empty
+        return value;
+
+    }
+
+    public static Optional<String> Of() {
+
+        //Used when the value must not be null, enforcing this constraint at runtime.
+
+        Optional<String> value = Optional.of("Hello"); // --> Optional[Hello]
+        //Optional<String> value=  Optional.of(null); // ----> NullPointerException
+        return value;
+
+    }
+    public static Optional<String> empty() {
+        return Optional.empty();
+
+    }
 }
+
